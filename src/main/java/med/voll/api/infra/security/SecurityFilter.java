ec0f2203f05a28570b6fbd4617ed41b511bef25f
Vitor Curtolo <vitor.curtolo09@gmail.com>
2024-05-21 20:09:12 -0300

    autenticando usuário novo

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -32,6 +32,7 @@ public class SecurityFilter extends OncePerRequestFilter {
 
             var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
+            System.out.println("logado");
         }
 
         filterChain.doFilter(request, response);
